8b96e83a6f166c6476b3da540d8eced19f8c2103
Kazim Walji <kwalji@bsd220.org>
2019-12-03 14:22:51 -0600

    Initial Commit

---
+++ topstories2Tests/topstories2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  topstories2Tests.swift
+//  topstories2Tests
+//
+//  Created by Kazim Walji on 12/3/19.
+//  Copyright © 2019 Mahdi Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import topstories2
+
+class topstories2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

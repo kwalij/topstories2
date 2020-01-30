470a1dc6ba2902193df3a1a3565bd18ffe7ea687
Kazim Walji <kwalji@bsd220.org>
2020-01-30 10:49:40 -0600

    works news

--- topstories2/SourcesViewController.swift
+++ topstories2/SourcesViewController.swift
@@ -9,12 +9,78 @@
 import UIKit
 
 class SourcesViewController : UITableViewController {
-
+    
+    var sources = [[String: String]]()
+   let apiKey = "5d892509a49046a087917c466fa80d09"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async {
+           [unowned self] in
+                    if let url = URL(string: query) {
+                        if let data = try? Data(contentsOf: url) {
+                            let json = try! JSON(data: data)
+                            if json["status"] == "ok" {
+                                self.parse(json: json)
+                                return
+                            }
+                        }
+                    }
+            self.loadError()
+        }
+        
     }
-
-
+    func parse(json: JSON)
+    {
+        for result in json["sources"].arrayValue
+        {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            sources.append(source)
+        }
+        DispatchQueue.main.async {
+           [unowned self] in
+              self.tableView.reloadData()
+        }
+    }
+    func loadError() {
+        DispatchQueue.main.async {
+           [unowned self] in
+        
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default, handler: nil))
+        
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        print(cell)
+        return cell
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let dvc = segue.destination as! ArticlesViewController
+                let index = tableView.indexPathForSelectedRow?.row
+        dvc.source = sources[index!]
+        dvc.apiKey = apiKey
+    }
+    
+    
 }
 
